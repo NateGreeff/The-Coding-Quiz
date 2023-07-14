1866cbdbd9c4c89d2fc946cdd8295ef703c6623b
Nathan Greeff <NGreeff@slco.org>
2023-07-13 22:17:19 -0600

    Made a list of quiz questions in js file

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+const quizQuestions = [
+    {
+      question: "Which is the correct way to write a comment in JavaScript?",
+      choices: ["{# ... #}", "<!--- .... ---!>", "// ....", "\\\\ ...."],
+      answer: "// ...."
+    },
+    {
+      question: "Which of the following is the correct syntax to redirect a URL using JavaScript?",
+      choices: ["document.location='http://www.example.com';", "navigator.location='http://www.example.com';", "window.location='http://www.example.com';", "browser.location='http://www.example.com';"],
+      answer: "window.location='http://www.example.com';"
+    },
+    {
+      question: "Inside which element do you put JavaScript?",
+      choices: ["<var>", "<script>", "<section>", "<code>"],
+      answer: "<script>"
+    },
+    {
+      question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+      choices: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script file='xxx.js'>"],
+      answer: "<script src='xxx.js'>"
+    },
+    {
+      question: "How do you write 'Hello World' in an alert box?",
+      choices: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');"],
+      answer: "alert('Hello World');"
+    },
+    {
+      question: "How do you create a function in JavaScript?",
+      choices: ["function myFunction()", "function:myFunction()", "function = myFunction()", "function => myFunction()"],
+      answer: "function myFunction()"
+    },
+    {
+      question: "How do you call a function named 'myFunction'?",
+      choices: ["call function myFunction()", "myFunction()", "call myFunction()", "call myFunction"],
+      answer: "myFunction()"
+    },
+    {
+      question: "How to write an IF statement in JavaScript?",
+      choices: ["if i = 5 then", "if i == 5 then", "if (i == 5)", "if i = 5"],
+      answer: "if (i == 5)"
+    },
+    {
+      question: "How does a WHILE loop start?",
+      choices: ["while (i <= 10)", "while i = 1 to 10", "while (i <= 10; i++)", "while (i = 0; i <= 10; i++)"],
+      answer: "while (i <= 10)"
+    },
+    {
+      question: "How does a FOR loop typically start?",
+      choices: ["for (i = 0; i <= 5)", "for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i <= 5; i++)"],
+      answer: "for (i = 0; i <= 5; i++)"
+    },
+    {
+      question: "How do you round the number 7.25, to the nearest integer?",
+      choices: ["Math.round(7.25)", "Math.rnd(7.25)", "round(7.25)", "rnd(7.25)"],
+      answer: "Math.round(7.25)"
+    },
+    {
+      question: "How do you find the number with the highest value of x and y?",
+      choices: ["Math.max(x, y)", "Math.ceil(x, y)", "ceil(x, y)", "top(x, y)"],
+      answer: "Math.max(x, y)"
+    },
+  ];
+
+  console.log(quizQuestions);
