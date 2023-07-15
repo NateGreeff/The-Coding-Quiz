4196987144ea84b6e3c3963c31884e71ed81ef0e
Nathan Greeff <NGreeff@slco.org>
2023-07-14 19:35:52 -0600

    Updated game logic finishing the html, css, and js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 // Quiz Questions
 const quizQuestions = [
     {
-      question: "Which is the correct way to write a comment in JavaScript?",
+      question: "Which is a correct way to write a comment in JavaScript?",
       choices: ["{# ... #}", "<!--- .... ---!>", "// ....", "\\\\ ...."],
       answer: "// ...."
     },
@@ -49,8 +49,8 @@ const quizQuestions = [
     },
     {
       question: "How does a FOR loop typically start?",
-      choices: ["for (i = 0; i <= 5)", "for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i <= 5; i++)"],
-      answer: "for (i = 0; i <= 5; i++)"
+      choices: ["for (var i = 0; i <= 5)", "for (var i = 0; i <= 5; i++)", "for var i = 1 to 5", "for (var i <= 5; i++)"],
+      answer: "for (var i = 0; i <= 5; i++)"
     },
     {
       question: "How do you round the number 7.25, to the nearest integer?",
@@ -62,30 +62,126 @@ const quizQuestions = [
       choices: ["Math.max(x, y)", "Math.ceil(x, y)", "ceil(x, y)", "top(x, y)"],
       answer: "Math.max(x, y)"
     },
+    {
+      question: "Which event occurs when the user clicks on an HTML element?",
+      choices: ["onmouseclick", "onchange", "onmouseover", "onclick"],
+      answer: "onclick"
+    },
+    {
+      question: "How do you declare a JavaScript variable?",
+      choices: ["variable carName;", "var carName;", "v carName;", "variable = carName;"],
+      answer: "var carName;"
+    },
+    {
+      question: "Which operator is used to assign a value to a variable?",
+      choices: ["-", "*", "=", "x"],
+      answer: "="
+    },
+    {
+      question: "What will the following code return: Boolean(10 > 9)",
+      choices: ["true", "false", "NaN", "undefined"],
+      answer: "true"
+    },
+    {
+      question: "Is JavaScript case-sensitive?",
+      choices: ["Yes", "No", "Sometimes", "Depends"],
+      answer: "Yes"
+    },
 ];
 
 // Element Connections
 const startButton = document.getElementById("startButton");
+const countDownText = document.getElementById("countDownText")
 const questionZone = document.getElementById("questionZone");
-const answerZone = document.getElementById("aswerZone");
+const answerZone = document.getElementById("answerZone");
+const scoreText = document.getElementById("score");
 const answerButtons = document.getElementsByClassName("answerBtn");
+const feedbackText = document.getElementById("answerFeedback");
+const highScoreText = document.getElementById("highScore");
 
-const gameTime = 3; //Total Game Time
+const gameTime = 50; //Total Game Time
 
 var questionIndex = 0;
 var timeleft = gameTime;
+var gameScore = 0;
+var highScore = localStorage.getItem('highScore') || 0;
+
+
+
+function generateAnswerBtn() {
+// This function creates and displays the answer buttons.
+// The question is retrieved from the quizQuestions array, and the answer buttons are created.
+// The answer buttons are added to the answerZone section.
+// The addEventListeners() function is called to add event listeners to the answer buttons.
+    var nextQ = quizQuestions[questionIndex];
+    for (var i = 0; i < nextQ.choices.length; i++) {
+        var choiceButton = document.createElement("button");
+        questionZone.textContent = nextQ.question;
+        choiceButton.textContent = nextQ.choices[i];
+        choiceButton.setAttribute("class", "answerBtn");
+        choiceButton.setAttribute("value", nextQ.choices[i]);
+        answerZone.appendChild(choiceButton);
+    }
+    addEventListeners();
+}
+
+function addEventListeners() {
+// This function adds event listeners to the answer buttons.
+// The event listener will be triggered when the user clicks on an answer button.
+// The event listener will check if the user's answer is correct.
+// If the answer is correct, the question index is incremented and the score is incremented.
+// If the answer is incorrect, the timer is decremented by 5 seconds.
+      for (var i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].addEventListener("click", function(event) {
+          if (event.target.value === quizQuestions[questionIndex].answer) {
+            questionIndex++;
+            answerZone.innerHTML = "";
+            gameScore += 1;
+            feedbackText.style.color = "green";
+            feedbackText.textContent = "Correct! Your score is " + gameScore;
+            if (questionIndex === quizQuestions.length) {
+              questionZone.textContent = "You got them all! Your score is " + gameScore;
+              answerZone.innerHTML = "";
+              feedbackText.textContent = "";
+              timeleft = 0;
+            }
+            else {
+              generateAnswerBtn();
+            }
+          } else {
+            timeleft -= 5;
+            feedbackText.style.color = "red";
+            feedbackText.textContent = "Wrong! -5 seconds on the timer!";
+          }
+        });
+      }
+  };
 
 startButton.addEventListener("click", function() {
-    startButton.style.display = 'none'; //Hide the start button
-    gameTimer = setInterval(function() {
-        document.getElementById("countDownText").innerHTML = timeleft + "</br></br> seconds left";
+// This function is triggered when the user clicks on the start button.
+// The game timer is started, and the first question is displayed.
+// The addEventListeners() function is called to add event listeners to the start button.
+    var gameTimer = setInterval(function() {
+        startButton.style.display = 'none'; //Hide the start button
+        countDownText.innerHTML = timeleft + "</br></br> seconds left";
         timeleft -= 1;
         if (timeleft < 0) {
             clearInterval(gameTimer);
-            document.getElementById("countDownText").textContent = "~Finished!~";
+            countDownText.textContent = "~Finished!~";
             startButton.textContent = "Try Again?"; 
             startButton.style.display = 'inline'; //Re-display start button
             timeleft = gameTime;
+            questionIndex = 0;
+            scoreText.textContent = `Score:  ${gameScore}`;
+            answerZone.innerHTML = "";
+            feedbackText.textContent = "";
+            if (gameScore > highScore) {
+                highScore = gameScore;
+                localStorage.setItem('highScore', highScore);
+            }
+            highScoreText.textContent = `High Score:  ${highScore}`;
+            gameScore = 0;
         }
     }, 1000);
+    generateAnswerBtn();
 });
